b4acba72b256b9affbf8bb30c08d0ba5413fed58
ayaanhassan <ayaanhassankhan.2c.j2@gmail.com>
2024-09-01 02:04:30 +0500

    Improved JavaScript files and connected hoodies.js

--- js/three.js
+++ js/three.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 600, 400 );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
